dc1a646954ef564077c2f341e8688cb95413fae7
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-07-25 13:10:26 +0530

    Placeholder text replacement

--- components/purple_header_component_1721893217.js
+++ components/purple_header_component_1721893217.js
@@ -15,11 +15,11 @@ Vue.component("purple_header_component_1721893217", {
                 <nav id="desktop-navigation" class="hidden md:flex md:grow">
     
                     <ul id="desktop-menu-links" class="flex grow justify-center flex-wrap items-center">
-                        <li><a id="HEADER_NAVBAR_TEXT_1-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" :class="[textStylePrimary, hoverTextStylePrimary]" href="HEADER_NAVBAR_TEXT_1.html">HEADER_NAVBAR_TEXT_1</a></li>
-                        <li><a id="HEADER_NAVBAR_TEXT_2-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="HEADER_NAVBAR_TEXT_2.html" :class="[textStylePrimary, hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_2</a></li>
-                        <li><a id="HEADER_NAVBAR_TEXT_3-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="HEADER_NAVBAR_TEXT_3.html" :class="[textStylePrimary, hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_3</a></li>
-                        <li><a id="HEADER_NAVBAR_TEXT_4-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="HEADER_NAVBAR_TEXT_4.html" :class="[textStylePrimary, hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_4</a></li>
-                        <li><a id="HEADER_NAVBAR_TEXT_5-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="HEADER_NAVBAR_TEXT_5.html" :class="[textStylePrimary, hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_5</a></li>
+                        <li><a id="Explore Blockchain-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" :class="[textStylePrimary, hoverTextStylePrimary]" href="Explore Blockchain.html">Explore Blockchain</a></li>
+                        <li><a id="Realtime Ethereum Monitoring-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="Realtime Ethereum Monitoring.html" :class="[textStylePrimary, hoverTextStylePrimary]">Realtime Ethereum Monitoring</a></li>
+                        <li><a id="Transaction Analytics-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="Transaction Analytics.html" :class="[textStylePrimary, hoverTextStylePrimary]">Transaction Analytics</a></li>
+                        <li><a id="Smart Contract Auditing-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="Smart Contract Auditing.html" :class="[textStylePrimary, hoverTextStylePrimary]">Smart Contract Auditing</a></li>
+                        <li><a id="Developer Tools-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="Developer Tools.html" :class="[textStylePrimary, hoverTextStylePrimary]">Developer Tools</a></li>
                     </ul>
     
                 </nav>
@@ -42,11 +42,11 @@ Vue.component("purple_header_component_1721893217", {
     
                     <nav id="mobile-nav" class="absolute top-full z-20 left-0 w-full px-4 sm:px-6 overflow-hidden transition-all duration-300 ease-in-out" x-ref="mobileNav" :style="expanded ? 'max-height: ' + $refs.mobileNav.scrollHeight + 'px; opacity: 1' : 'max-height: 0; opacity: .8'" @click.outside="expanded = false" @keydown.escape.window="expanded = false" x-cloak>
                         <ul id="mobile-nav-list" class="border border-transparent rounded-lg px-4 py-1.5" :class="signUpButtonGradient">
-                            <li><a id="mobile-HEADER_NAVBAR_TEXT_1-link" class="flex font-medium text-sm py-1.5" href="HEADER_NAVBAR_TEXT_1.html" :class="[textStylePrimary,hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_1</a></li>
-                            <li><a id="mobile-HEADER_NAVBAR_TEXT_2-link" class="flex font-medium text-sm py-1.5" href="HEADER_NAVBAR_TEXT_2.html" :class="[textStylePrimary,hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_2</a></li>
-                            <li><a id="mobile-HEADER_NAVBAR_TEXT_3-link" class="flex font-medium text-sm py-1.5" href="HEADER_NAVBAR_TEXT_3.html" :class="[textStylePrimary,hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_3</a></li>
-                            <li><a id="mobile-HEADER_NAVBAR_TEXT_4-link" class="flex font-medium text-sm py-1.5" href="HEADER_NAVBAR_TEXT_4.html" :class="[textStylePrimary,hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_4</a></li>
-                            <li><a id="mobile-HEADER_NAVBAR_TEXT_5-link" class="flex font-medium text-sm py-1.5" href="HEADER_NAVBAR_TEXT_5.html" :class="[textStylePrimary,hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_5</a></li>
+                            <li><a id="mobile-Explore Blockchain-link" class="flex font-medium text-sm py-1.5" href="Explore Blockchain.html" :class="[textStylePrimary,hoverTextStylePrimary]">Explore Blockchain</a></li>
+                            <li><a id="mobile-Realtime Ethereum Monitoring-link" class="flex font-medium text-sm py-1.5" href="Realtime Ethereum Monitoring.html" :class="[textStylePrimary,hoverTextStylePrimary]">Realtime Ethereum Monitoring</a></li>
+                            <li><a id="mobile-Transaction Analytics-link" class="flex font-medium text-sm py-1.5" href="Transaction Analytics.html" :class="[textStylePrimary,hoverTextStylePrimary]">Transaction Analytics</a></li>
+                            <li><a id="mobile-Smart Contract Auditing-link" class="flex font-medium text-sm py-1.5" href="Smart Contract Auditing.html" :class="[textStylePrimary,hoverTextStylePrimary]">Smart Contract Auditing</a></li>
+                            <li><a id="mobile-Developer Tools-link" class="flex font-medium text-sm py-1.5" href="Developer Tools.html" :class="[textStylePrimary,hoverTextStylePrimary]">Developer Tools</a></li>
                         </ul>
                     </nav>
     
